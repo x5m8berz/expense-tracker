3a03e665c823687645327ab8f769c4c7576f0ea6
Hanrocky <hanrocky.halim@gmail.com>
2022-05-31 23:16:28 +0700

    Third Commit
    Add context and reducer

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React,{ createContext, useReducer } from "react";
+import AppReducer from './AppReducer.js';
 
 // initial state
 
@@ -12,5 +13,15 @@ const initialState = {
 }
 
 // Create context
-
 export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({children}) => {
+    const [state,dispatch] = useReducer(AppReducer, initialState);
+
+    return (<GlobalContext.Provider value={{
+        transactions:state.transaction
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
